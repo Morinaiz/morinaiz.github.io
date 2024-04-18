fb4ed023fcac5593f69f5436863f68c0f36c68a7
Lorenzo Morini <lorenzo.morini99@gmail.com>
2024-04-18 02:29:43 +0200

    Added Double Face Shader Upwork Project

--- gallery.js
+++ gallery.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let columns = [];
     let columnHeights = [];
     let currentColumnsCount;
-    let currentFolderIndex = 5;
+    let currentFolderIndex = 6;
 
     function setupColumns(numColumns) {
         currentColumnsCount = numColumns;

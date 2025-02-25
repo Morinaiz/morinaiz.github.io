edeb7e14af22a71c5d103d68bd7769c17c989729
Lorenzo Morini <lorenzo.morini99@gmail.com>
2025-02-25 01:13:38 +0100

    Added third blog post, let's go!

--- blog-gallery.js
+++ blog-gallery.js
@@ -2,7 +2,7 @@ let overlayOpen = false;
 
 document.addEventListener("DOMContentLoaded", function () {
     const gallery = document.getElementById("blog-gallery");
-    let currentBlogPostIndex = 2;
+    let currentBlogPostIndex = 3;
 
     function setupColumn() {
         gallery.innerHTML = '';
